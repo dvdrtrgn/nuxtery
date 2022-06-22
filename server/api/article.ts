7853790ed642a3740f3d379a4a5a12a58a8e7ca8
dturgeon <dturgeon@bizjournals.com>
2022-06-22 11:30:24 -0400

    Fake article endpoint

---
+++ server/api/article.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+import type { IncomingMessage, ServerResponse } from 'http';
+import data from '~/data/potentialfullreturn.json';
+
+export default async (req: IncomingMessage, res: ServerResponse) => {
+    const dataStr = JSON.stringify(data.article);
+    // if (query) {
+    //     try {
+    //         apiData = await $fetch(`${baseUrl}${query}`);
+    //     } catch (err) {
+    //         console.warn(err);
+    //     }
+    // }
+
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(dataStr);
+    res.end();
+};
