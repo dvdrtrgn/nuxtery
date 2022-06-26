ba7e61e42c2f082b328d3ff6ba923b2190840766
dvdrtrgn <dvdrtrgn@gmail.com>
2022-06-25 23:27:13 -0500

    Refactor

--- composables/useMouseClient.ts
+++ composables/useMouseClient.ts
@@ -1,16 +1,32 @@
 import { ref, onMounted, onUnmounted } from 'vue';
 
-export default function useMyMouse () {
+export default function useMyMouse (options:any = {}) {
     const x = ref(0);
     const y = ref(0);
+    const {
+        eventFilter,
+    } = options;
+    const calls = ref(0);
+    const count = ref(0);
+    const accuracy = computed(
+        () => (count.value / calls.value * 100) | 0,
+    );
 
     function updateMouse (evt:MouseEvent) {
         x.value = evt.clientX;
         y.value = evt.clientY;
+        count.value++;
     }
 
-    onMounted(() => window.addEventListener('mousemove', updateMouse));
-    onUnmounted(() => window.removeEventListener('mousemove', updateMouse));
+    const updateMouseWrapper = (event:MouseEvent) => {
+        calls.value++;
+        return eventFilter === undefined
+            ? updateMouse(event)
+            : eventFilter(() => updateMouse(event), {} as any);
+    };
 
-    return { x, y };
+    onMounted(() => window.addEventListener('mousemove', updateMouseWrapper));
+    onUnmounted(() => window.removeEventListener('mousemove', updateMouseWrapper));
+
+    return { x, y, calls, count, accuracy };
 }
