db47323210a9a808171356f9264aa8e3c8ac2e37
dvdrtrgn <dvdrtrgn@gmail.com>
2022-07-02 11:12:44 -0500

    Move ready logging

--- composables/useRuntimeFED.ts
+++ composables/useRuntimeFED.ts
@@ -1,8 +1,6 @@
 import { stringify } from '../utils/circular-json.mjs'; // stringify is probably unneccessary here
 
 export default function () {
-    console.log('Ready on', 'http://localhost:3000/');
-
     const config = useRuntimeConfig();
     return stringify(config);
 };
---
+++ server/api/_ready.ts
@@ -0,0 +1,3 @@
+console.log('Ready on http://localhost:3000');
+
+export default () => Date.now();
