aa1ee612f3e49f901d16f58c5a9f61c3022db066
dvdrtrgn <dvdrtrgn@gmail.com>
2022-07-06 21:01:37 -0500

    Reorg data

---
+++ data/sample1/index.js
@@ -0,0 +1,24 @@
+import authors from './article/authors.js';
+import content from './article/content.js';
+import leadins from './article/leadins.js';
+
+import journal from './business/journal.js';
+import market from './business/market.js';
+
+import metadata from './other/metadata.js';
+import randos from './other/randos.js';
+
+// match potentialfullreturn.json
+export default {
+    article: {
+        authors,
+        content,
+        journal,
+        market,
+        leadins,
+        metadata: {
+            data: metadata,
+        },
+        ...randos,
+    },
+};
--- server/api/tidy/metadata.ts
+++
@@ -1,4 +0,0 @@
-import pickResponse from '~/server/utils/pickResponse';
-import { metadata } from '~/data/potentialfullreturn-tidy.json';
-
-export default pickResponse(metadata);
