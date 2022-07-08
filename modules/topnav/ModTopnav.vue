2bd3f120fe1fbb6046468592f48a3d9825e6cf30
dturgeon <dturgeon@bizjournals.com>
2022-07-08 14:31:55 -0400

    Part out main chunks

--- modules/topnav/ModTopnav.vue
+++ modules/topnav/ModTopnav.vue
@@ -3,16 +3,44 @@ const props = defineProps({
     data: { type: Object, required: true },
 });
 
-const data = props.data;
+const { topnav } = props.data;
 
 </script>
 
 <template>
     <div vue-article>
+        <PreCollapse :data="topnav">
+            <b>All data</b>
+        </PreCollapse>
+
         <div dev-flex>
             <div dev-inset>
-                <PreCollapse :data="data.topnav">
-                    <b>Top nav</b>
+                <PreCollapse :data="topnav.topItems.items">
+                    <b>Top items / items</b>
+                </PreCollapse>
+
+                <PreCollapse :data="topnav.sections">
+                    <b>Top sections</b>
+                </PreCollapse>
+
+                <PreCollapse :data="topnav.sections.industries">
+                    <b>Top sections / industries</b>
+                </PreCollapse>
+
+                <PreCollapse :data="topnav.sections.sponsored_content">
+                    <b>Top sections / sponsored_content</b>
+                </PreCollapse>
+
+                <PreCollapse :data="topnav.sections.events">
+                    <b>Top sections / events</b>
+                </PreCollapse>
+
+                <PreCollapse :data="topnav.sections.products">
+                    <b>Top sections / products</b>
+                </PreCollapse>
+
+                <PreCollapse :data="topnav.sections.publications">
+                    <b>Top sections / publications</b>
                 </PreCollapse>
             </div>
         </div>
