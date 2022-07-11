3982c16e529c1b86809dcf2dd6d3833d603832d0
dturgeon <dturgeon@bizjournals.com>
2022-07-11 14:07:24 -0400

    Add city dropdown

---
+++ modules/topnav/components/Global/GlobalSiteNav.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+// const props = defineProps({
+//     data: { type: Object, required: true },
+// });
+
+// DATA
+import citydata from '~/data/city-dropdown-data';
+
+</script>
+
+<template>
+    <div id="SiteNav" dev-flex>
+        <GlobalSiteNavBranding></GlobalSiteNavBranding>
+
+        <GlobalSelectCityDropdown
+            :data="citydata"
+        ></GlobalSelectCityDropdown>
+
+        <GlobalAccountDropdown></GlobalAccountDropdown>
+    </div>
+</template>
+
+<!--
+<style lang="scss">
+</style>
+-->
---
+++ modules/topnav/components/Global/SelectCity/GlobalSelectCityItem.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+/*
+*/
+const props = defineProps({
+    data: { type: Object, required: true },
+});
+</script>
+
+<template>
+    <div>
+        <a
+            :href="props.data.href"
+            data-ct="Nav : Select City : {{ props.data.name}}"
+            data-cy="market-nav-{{ props.data.id}}"
+            class="dropdown-item p-0 m-0"
+        >
+            {{ props.data.name }}
+        </a>
+    </div>
+</template>
+
+<!--
+<style lang="scss">
+</style>
+-->
