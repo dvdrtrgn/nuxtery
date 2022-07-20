8f9c7d6427ab51fc01c4824b2fbedde15b10b92f
dturgeon <dturgeon@bizjournals.com>
2022-07-20 09:42:12 -0400

    Start link list

--- modules/footer/components/FooterBody/FooterBodyAbout.vue
+++ modules/footer/components/FooterBody/FooterBodyAbout.vue
@@ -1,47 +1,46 @@
 <script setup lang="ts">
-/*
-const props = defineProps({
-    data: { type: Object, required: true },
-});
-*/
+
+const data = [
+    {
+        ct: '2016 Footer : About The Business Journals',
+        href: 'https://www.bizjournals.com/about-us/',
+        text: ' About The Business Journals',
+    },
+    {
+        ct: '2016 Footer : Advertise',
+        href: 'https://advertise.bizjournals.com?ana=om_wb_dg_ftr',
+        text: ' Advertise',
+    },
+    {
+        ct: '2016 Footer : Help &amp; FAQs',
+        href: 'https://support.bizjournals.com/en/',
+        text: ' Help &amp; FAQs',
+    },
+    {
+        ct: '2016 Footer : Circulation Sales Center Directory',
+        href: 'https://www.bizjournals.com/about-us/call-center',
+        text: 'Circulation Sales Center Directory',
+    },
+];
+
 </script>
 
 <template>
     <div>
-        <div class="1/1 md__1/4">
+        <div class="">
             <h4 class="link-list-title">
                 About &amp; Contact
             </h4>
             <ul class="link-list">
-                <li data-ct="2016 Footer : About The Business Journals">
-                    <a href="https://www.bizjournals.com/about-us/">
-                        <span class="link-list-item__title">About The
-                            Business Journals</span>
-                    </a>
-                </li>
-                <li data-ct="2016 Footer : Advertise">
-                    <a
-                        href="https://advertise.bizjournals.com?ana=om_wb_dg_ftr"
-                    >
-                        <span
-                            class="link-list-item__title"
-                        >Advertise</span>
-                    </a>
-                </li>
-                <li data-ct="2016 Footer : Help &amp; FAQs">
-                    <a href="https://support.bizjournals.com/en/">
-                        <span class="link-list-item__title">Help &amp;
-                            FAQs</span>
-                    </a>
-                </li>
                 <li
-                    data-ct="2016 Footer : Circulation Sales Center Directory"
+                    v-for="item in data"
+                    :key="item.text"
+                    :data-ct="item.ct"
                 >
-                    <a
-                        href="https://www.bizjournals.com/about-us/call-center"
-                    >
-                        <span class="link-list-item__title">Circulation
-                            Sales Center Directory</span>
+                    <a :href="item.href">
+                        <span class="link-list-item__title">
+                            {{ item.text }}
+                        </span>
                     </a>
                 </li>
             </ul>
--- modules/footer/components/FooterBody/FooterBodyAcbj.vue
+++ modules/footer/components/FooterBody/FooterBodyAcbj.vue
@@ -17,43 +17,44 @@ const props = defineProps({
                     data-ct="2016 Footer : American City Business Journals"
                 >
                     <a href="http://www.acbj.com/">
-                        <span class="link-list-item__title">American
-                            City Business Journals</span>
+                        <span class="link-list-item__title">
+                            American City Business Journals
+                        </span>
                     </a>
                 </li>
                 <li data-ct="2016 Footer : AmericanInno">
                     <a href="https://www.bizjournals.com/inno">
-                        <span
-                            class="link-list-item__title"
-                        >AmericanInno</span>
+                        <span class="link-list-item__title">
+                            AmericanInno
+                        </span>
                     </a>
                 </li>
                 <li data-ct="2016 Footer : BizEquity">
                     <a href="https://www.bizequity.com/">
-                        <span
-                            class="link-list-item__title"
-                        >BizEquity</span>
+                        <span class="link-list-item__title">
+                            BizEquity
+                        </span>
                     </a>
                 </li>
                 <li data-ct="2016 Footer : BizLeads">
                     <a href="https://www.bizleads.com/">
-                        <span
-                            class="link-list-item__title"
-                        >BizLeads</span>
+                        <span class="link-list-item__title">
+                            BizLeads
+                        </span>
                     </a>
                 </li>
                 <li data-ct="2016 Footer : Bizwomen">
                     <a href="https://www.bizjournals.com/bizwomen">
-                        <span
-                            class="link-list-item__title"
-                        >Bizwomen</span>
+                        <span class="link-list-item__title">
+                            Bizwomen
+                        </span>
                     </a>
                 </li>
                 <li data-ct="2016 Footer : Hemmings">
                     <a href="http://www.hemmings.com/">
-                        <span
-                            class="link-list-item__title"
-                        >Hemmings</span>
+                        <span class="link-list-item__title">
+                            Hemmings
+                        </span>
                     </a>
                 </li>
             </ul>
