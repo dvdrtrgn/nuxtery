0d9643f87c7f53e896d65c8059336745cf0b513d
dturgeon <dturgeon@bizjournals.com>
2022-07-22 16:27:42 -0400

    Finish browserdump

--- components/BrowserDump/BrowserDump.vue
+++ components/BrowserDump/BrowserDump.vue
@@ -3,21 +3,51 @@
 const props = defineProps<{ label?: string }>();
 const flipper = useToggle(props.label);
 const data = {
-    ...props,
-    ...flipper,
+    ...toRefs(props),
+    ...toRefs(flipper),
 };
+
 </script>
+
 <template>
     <ClientOnly>
-        <button @click="flipper.handle">
-            {{ label || 'toggle' }}
-        </button>
+        <div class="browser-dump" :dev-active="flipper.active">
+            <button @click="flipper.handle">
+                {{ label || 'toggle' }}
+            </button>
 
-        <div
-            v-if="flipper.active"
-            dev-inset
-        >
-            <slot v-bind="data"></slot>
+            <div v-if="flipper.active" dev-inset>
+                <slot v-bind="data"></slot>
+            </div>
         </div>
     </ClientOnly>
 </template>
+
+<style lang="scss">
+.browser-dump {
+    $color: steelblue;
+
+    > button:hover {
+        color: $color;
+    }
+    &[dev-active=false] {
+        display: inline;
+    }
+    &[dev-active=true]  {
+        display: block;
+        > button {
+            border: 1px solid $color;
+            border-bottom: 1px solid white;
+            border-radius: 7px 7px 0rem 0rem;
+            color: $color;
+            font-size: 80%;
+            position: relative;
+            z-index: 1;
+        }
+        > div {
+            border: 1px solid $color;
+            transform: translateY(-1px);
+        }
+    }
+}
+</style>
