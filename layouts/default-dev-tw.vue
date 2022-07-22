722d8d6855ee62e57059450bf7222e346b3541f3
dturgeon <dturgeon@bizjournals.com>
2022-07-22 17:46:59 -0400

    Spread dev templates

---
+++ layouts/default-dev-tw.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import DefaultDevVue from './default-dev.vue';
+
+</script>
+
+<template>
+    <div dev-template>
+        <Script src="https://cdn.tailwindcss.com"></Script>
+        <DefaultDevVue><slot></slot></DefaultDevVue>
+    </div>
+</template>
---
+++ layouts/default-dev.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+import DefaultVue from './default.vue';
+
+</script>
+
+<template>
+    <div dev-template>
+        <DevHeader></DevHeader>
+
+        <DefaultVue><slot></slot></DefaultVue>
+
+        <DevFooter></DevFooter>
+    </div>
+</template>
+
+<style lang="scss">
+
+html {
+    $headerHeight: 7vh;
+    $footerHeight: 5vh;
+
+    body, [dev-template] {
+        margin: 0;
+        margin-top: $headerHeight * 1;
+        margin-bottom: $footerHeight * 1.2;
+    }
+    [dev-header], [dev-footer] {
+        left: 0;
+        position: fixed;
+        width: 100%;
+    }
+
+    [dev-header] {
+        top: 0;
+        height: $headerHeight;
+    }
+    [dev-footer] {
+        bottom: 0;
+        height: $footerHeight;
+    }
+}
+
+</style>
