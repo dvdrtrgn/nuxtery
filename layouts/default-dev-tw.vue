5aa4f81895be2ba49438a572013427ac544e3e88
dturgeon <dturgeon@bizjournals.com>
2022-07-22 18:04:43 -0400

    Tidy up more

--- layouts/default-dev-tw.vue
+++ layouts/default-dev-tw.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 
-import DefaultDevVue from './default-dev.vue';
+import DefaultDev from './default-dev.vue';
 
 </script>
 
 <template>
     <div dev-template>
         <Script src="https://cdn.tailwindcss.com"></Script>
-        <center><i>using cdn.tailwindcss.com</i></center>
-        <DefaultDevVue><slot></slot></DefaultDevVue>
+
+        <p class="text-center font-mono">
+            <i>injecting cdn.tailwindcss.com</i>
+        </p>
+
+        <DefaultDev><slot></slot></DefaultDev>
     </div>
 </template>
